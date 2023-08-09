988d9193b27cb9cabac8f881fe1ed25c1a89570b
“linusJtareby” <linus.tareby@se.experis.com>
2023-08-09 15:26:59 +0200

    first commit

---
+++ src/main/java/com/chinook/chinookjdbc/ChinookJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.chinook.chinookjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinookJdbcApplication.class, args);
+	}
+
+}
