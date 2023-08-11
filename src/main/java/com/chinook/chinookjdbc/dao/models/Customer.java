434192c6385b732bc60f52f0803a89153bc55b2e
“linusJtareby” <linus.tareby@se.experis.com>
2023-08-11 14:42:41 +0200

    newComments

--- src/main/java/com/chinook/chinookjdbc/dao/models/Customer.java
+++
@@ -1,6 +0,0 @@
-package com.chinook.chinookjdbc.dao.models;
-
-public record Customer(int id, String firstName, String lastName, String country, String postalCode, String email,
-                String phoneNr) {
-
-}
